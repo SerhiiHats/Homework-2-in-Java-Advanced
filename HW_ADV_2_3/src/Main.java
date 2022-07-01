5dc99df9b84efbc520b494aaf8047af297d9282d
Serhi Hats <gatsserv@gmail.com>
2022-07-02 01:00:45 +0300

    Add two commit in homework 2 at Java advanced

--- HW_ADV_2_3/src/Main.java
+++ HW_ADV_2_3/src/Main.java
@@ -1,72 +1,75 @@
 import color.Color;
-
 import java.util.*;
-/**Домашнее задание 2
-Задание 3
- Минимальное из N чисел(использовать LinkedList):
- 1.       Ввести с клавиатуры число N.
- 2.       Считать N целых чисел и заполнить ими список - метод getIntegerList.
- 3.       Найти минимальное число среди элементов списка - метод getMinimum.
+
+/**
+ * Домашнее задание 2
+ * Задание 3
+ * Минимальное из N чисел(использовать LinkedList):
+ * 1.       Ввести с клавиатуры число N.
+ * 2.       Считать N целых чисел и заполнить ими список - метод getIntegerList.
+ * 3.       Найти минимальное число среди элементов списка - метод getMinimum.
  */
 
 public class Main {
     public static void main(String[] args) {
-        List<Integer> list = new LinkedList<>();
-        long number_N = enterNumber();
-        getIntegerList(list, number_N);
-
+        LinkedList<Integer> list = new LinkedList<>();
+        System.out.print ("Введите количество цифр которые необходимо добавить в LinkedList: ");
+        int number = enterNumber();
+        getIntegerList(list, number);
         System.out.print("1. Выведем на экран заполненный LinkedList    : " + Color.GREEN);
         for (Integer g : list) {
             System.out.print(g + " ");
         }
-
         System.out.println();
         System.out.println(Color.DEFAULT + "2. Минимальное число в LinkedList             : " + Color.GREEN + getMinimum(list) + Color.DEFAULT);
 
-       // System.out.println();
-        System.out.print("3. Выведем на экран отсортированый LinkedList : " + Color.GREEN);
+    }
+    /**
+     * Первый вариант реализиции метода getMinimum.
+     */
+    private static int getMinimum(LinkedList<Integer> list) {
+        int minNumber = list.getFirst();
         for (Integer g : list) {
-            System.out.print(g + " ");
+            if (minNumber > g) {
+               minNumber = g;
+            }
         }
-        System.out.println(Color.DEFAULT);
-
+        return minNumber;
     }
 
-    private static long enterNumber() {
+    /**
+     * Второй вариант реализиции метода getMinimum.
+     */
+//    private static int getMinimum(LinkedList<Integer> list) {
+//        list.sort(new Comparator<Integer>() {
+//            @Override
+//            public int compare(Integer o1, Integer o2) {
+//                return o1 - o2;
+//            }
+//        });
+//        return list.getFirst();
+//    }
+
+    private static void getIntegerList(LinkedList<Integer> list, int number) {
         Scanner scanner = new Scanner(System.in);
-        long number;
-        System.out.println("Введите целое число:");
+        for (int i = 0; i < number; i++) {
+            System.out.print("Введите  " + Color.GREEN + (i + 1) + "-е " + Color.DEFAULT + "целое число: ");
+            list.add(enterNumber());
+        }
+        scanner.close();
+    }
+
+
+    private static int enterNumber() {
+        Scanner scanner = new Scanner(System.in);
         while (true) {
-            if (scanner.hasNextLong()) {
-                number = scanner.nextLong();
-                break;
+            if (scanner.hasNextInt()) {
+                return scanner.nextInt();
             } else {
-                System.out.println("Ошибка ввода целого числа, проверте и повторите");
+                System.out.print("Данные не приняты. Введите еще раз число: ");
                 scanner.next();
             }
         }
-        scanner.close();
-        return number;
-    }
 
-    private static int getMinimum(List<Integer> list) {
-        list.sort(new Comparator<Integer>() {
-            @Override
-            public int compare(Integer o1, Integer o2) {
-                return o1 - o2;
-            }
-        });
-        ListIterator iterator = list.listIterator();
-        while (iterator.hasNext()){
-            return (int)iterator.next();
-        }
-     return 0;
-    }
-
-    private static void getIntegerList(List<Integer> list, long chisloN) {
-        String str = String.valueOf(chisloN);
-        for (int i = 0; i < str.length(); i++) {
-            list.add(str.charAt(i) - '0');
-        }
     }
 }
